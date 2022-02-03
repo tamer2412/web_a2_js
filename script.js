1ce4197770afa7f06eb88c942ec506ea525b2a94
tamer2412 <96350687+tamer2412@users.noreply.github.com>
2022-02-03 21:39:45 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let today = new Date();
+const day = today.getDay();
+const weeks = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"];
+const year = today.getFullYear();
+const date = today.getDate();
+const month = today.getMonth() + 1;
+let time =  today.getHours() + ":" +  today.getMinutes() + ":" +  today.getSeconds();
+document.getElementById("datacheck").innerHTML = 'Year: ' + year + ' <br> Today is: ' + weeks[day] + ' <br> Date: ' + date + ' <br> Month: ' + month + ' <br> Current time is: ' + time;
+
+var dateNow = new Date("02/03/2022");
+var gr = new Date("05/25/2024");//idk correct date of graduation, so i picked default school graduation date
+var eqwe = gr.getTime() - dateNow.getTime();
+var res = eqwe / (1000 * 3600 * 24);
+document.getElementById("remain").innerHTML = (res > 1 ? res + " days" : res + " day") + "  left until the freedom!";
+function multiply() {
+    document.querySelector("#result").innerHTML = document.querySelector("#fNum").value * document.querySelector("#sNum").value;
+}
+
+function divide() {
+    document.querySelector("#result").innerHTML = document.querySelector("#fNum").value / document.querySelector("#sNum").value;
+}
